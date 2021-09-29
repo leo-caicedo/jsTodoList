0ac746b27bff9f7fc6b173a8c6d58f2bcc6c6ab9
Leonardo Caicedo <devleonardocaicedo@gmail.com>
2021-09-28 20:43:30 -0500

    Filtros

--- js/view.js
+++ js/view.js
@@ -1,16 +1,20 @@
 import AddTodo from "./components/add-todo.js";
 import Modal from "./components/modal.js";
+import Filters from "./components/filters.js";
 
 export default class View {
   constructor() {
     this.model = null;
     this.table = document.getElementById("table");
     this.addTodoForm = new AddTodo();
+    this.modal = new Modal();
+    this.filters = new Filters();
+
     this.addTodoForm.onClick((title, description) =>
       this.addTodo(title, description)
     );
-    this.modal = new Modal();
     this.modal.onClick((id, values) => this.editTodo(id, values));
+    this.filters.onClick((filters) => this.filter(filters));
   }
 
   setModel(model) {
@@ -20,6 +24,33 @@ export default class View {
   render() {
     const todos = this.model.getTodos();
     todos.forEach((todo) => this.createRow(todo));
+  }
+
+  filter(filters) {
+    const { type, words } = filters;
+    const [, ...rows] = this.table.getElementsByTagName("tr");
+    for (const row of rows) {
+      const [title, description, completed] = row.children;
+      let shouldHide = false;
+      if (words) {
+        shouldHide =
+          !title.innerText.includes(words) &&
+          !description.innerHTML.includes(words);
+      }
+
+      const shouldBeCompleted = type === "completed";
+      const isCompleted = completed.children[0].checked;
+
+      if (type !== "all" && shouldBeCompleted !== isCompleted) {
+        shouldHide = true;
+      }
+
+      if (shouldHide) {
+        row.classList.add("d-none");
+      } else {
+        row.classList.remove("d-none");
+      }
+    }
   }
 
   addTodo(title, description) {
@@ -61,7 +92,13 @@ export default class View {
     editBtn.innerHTML = '<i class="fa fa-pencil"></i>';
     editBtn.setAttribute("data-toggle", "modal");
     editBtn.setAttribute("data-target", "#modal");
-    editBtn.onclick = () => this.modal.setValues(todo);
+    editBtn.onclick = () =>
+      this.modal.setValues({
+        id: todo.id,
+        title: row.children[0].innerText,
+        description: row.children[0].innerText,
+        completed: row.children[2].children[0].checked,
+      });
     row.children[3].appendChild(editBtn);
 
     const removeBtn = document.createElement("button");
